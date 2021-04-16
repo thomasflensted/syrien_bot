2bc451b9a6a840c1df3085e79f2780763fb6c457
Thomas Flensted <thomasflenstedjensen@gmail.com>
2021-04-16 15:33:03 +0200

    skjult env variables ved brug af os.environ

--- syrien_bot.py
+++ syrien_bot.py
@@ -1,5 +1,9 @@
-import tweepy, time
-from credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+import tweepy, time, os
+
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
