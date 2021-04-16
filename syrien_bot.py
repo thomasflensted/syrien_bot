a6079dd3df756f98227590272234a8de8da9d7e4
Thomas Flensted <thomasflenstedjensen@gmail.com>
2021-04-16 17:19:10 +0200

    ændret på strengformateringen i tweet-teksten og tilføjfet nogle tags

--- syrien_bot.py
+++ syrien_bot.py
@@ -11,9 +11,9 @@ api = tweepy.API(auth)
 
 def main():
 
-    seconds_pr_min = 1
-    mins_pr_hour = 1
-    hours_pr_day = 5
+    seconds_pr_min = 60
+    mins_pr_hour = 60
+    hours_pr_day = 24
     one_day = seconds_pr_min * mins_pr_hour * hours_pr_day
 
     days_gone = 2 * 365
@@ -27,17 +27,19 @@ def main():
 
 def create_string(num):
 
+    tag_ministries = "#bringboernenehjem @statsmin @DanishMFA"
+
     if num % 365 == 0:
 
-        txt = "De danske børn har nu siddet mindst {} år i Syrien og de er stadig ikke på vej hjem. #bringboernenehjem".format(int(num / 365))
+        txt = "De danske børn har nu siddet mindst {years} år i lejrene i Syrien. {tags}".format(years = int(num / 365), tags = tag_ministries)
 
     elif num % 365 == 1:
 
-        txt = "De danske børn har nu siddet mindst {} år og 1 dag i Syrien og de er stadig ikke på vej hjem. #bringboernenehjem".format(int(num / 365))
+        txt = "De danske børn har nu siddet mindst {years} år og 1 dag i lejrene i Syrien. {tags}".format(int(num / 365), tags = tag_ministries)
 
     else:
 
-        txt = "De danske børn har nu siddet mindst {years} år og {days} dage i Syrien og de er stadig ikke på vej hjem. #bringboernenehjem".format(years =  int(num / 365), days = num % 365)
+        txt = "De danske børn har nu siddet mindst {years} år og {days} dage i lejrene i Syrien. {tags}".format(years =  int(num / 365), days = num % 365, tags = tag_ministries)
 
     return txt
 
